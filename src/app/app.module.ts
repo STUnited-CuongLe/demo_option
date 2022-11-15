4f34efe4bc7132c4cd28b980ffa437733c8491ba
Le Viet Cuong <cuong.le@stunited.vn>
2022-11-15 07:51:43 +0700

    option1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OptionOneComponent } from './option-one/option-one.component';
+
+import { FormsModule } from '@angular/forms';
+import {CalendarModule} from 'primeng/calendar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OptionOneComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalendarModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/option-one/option-one.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-option-one',
+  templateUrl: './option-one.component.html',
+  styleUrls: ['./option-one.component.css'],
+})
+export class OptionOneComponent implements OnInit {
+  date3: Date;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
